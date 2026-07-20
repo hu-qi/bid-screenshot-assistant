f2d0e9457e84603f5bf6ee0caced509f20306560
huqi <huqi1024@gmail.com>
2026-07-20 16:25:11 +0800

    extend browser profile tests for Guangdong government procurement

--- tests/test_browser_profiles.py
+++ tests/test_browser_profiles.py
@@ -6,6 +6,7 @@ from bid_screenshot_assistant.adapters.profiles import (
     CHINA_MOBILE_PROFILE,
     CHINA_TOWER_EPROC_PROFILE,
     CHINA_UNICOM_PROFILE,
+    GD_GP_PROFILE,
 )
 from bid_screenshot_assistant.domain.models import PlatformId
 from bid_screenshot_assistant.services.url_guard import (
@@ -21,6 +22,7 @@ def test_priority_profiles_are_unique_and_https():
         PlatformId.CHINA_UNICOM,
         PlatformId.CHINA_TOWER_EPROC,
         PlatformId.CEBPUBSERVICE,
+        PlatformId.GD_GP,
     }
     for profile in BROWSER_PROFILES.values():
         assert profile.start_url.startswith("https://")
@@ -66,6 +68,17 @@ def test_ceb_detail_url_patterns(url: str):
     assert validate_detail_url(CEBPUBSERVICE_PROFILE, url) == url
 
 
+@pytest.mark.parametrize(
+    "url",
+    [
+        "https://gdgpo.czt.gd.gov.cn/gpcms/rest/web/v2/info/getInfoById?id=19249ba9-5d33-48e4-b9cb-def134be8824",
+        "https://gdgpo.czt.gd.gov.cn/articleGd?type=article&id=19249ba9-5d33-48e4-b9cb-def134be8824&channelName=%E9%87%87%E8%B4%AD%E7%BB%93%E6%9E%9C%E5%85%AC%E5%91%8A",
+    ],
+)
+def test_gd_gp_detail_url_patterns(url: str):
+    assert validate_detail_url(GD_GP_PROFILE, url) == url
+
+
 def test_deceptive_and_external_hosts_are_rejected():
     for url in (
         "https://ebid.chinatowercom.cn.evil.example/zgtt/gggs/003001/detail.html",
@@ -83,3 +96,5 @@ def test_arbitrary_urls_are_rejected():
         validate_detail_url(CHINA_MOBILE_PROFILE, "https://b2b.10086.cn/anything")
     with pytest.raises(UnsafeNavigationError):
         validate_detail_url(CEBPUBSERVICE_PROFILE, "https://bulletin.cebpubservice.com/anything")
+    with pytest.raises(UnsafeNavigationError):
+        validate_detail_url(GD_GP_PROFILE, "https://gdgpo.czt.gd.gov.cn/anything")
